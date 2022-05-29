5fae2b803e9db0c706140cbfc279166161b2052d
Ceylo <lucas.soltic@orange.fr>
2022-05-29 17:59:56 +0200

    Documentation

--- Sources/ListItemTracking/ListItemTracking.swift
+++ Sources/ListItemTracking/ListItemTracking.swift
@@ -50,10 +50,30 @@ struct ListFrameTracking: ViewModifier {
 }
 
 public extension View {
+    
+    /// Add an action to perform when a List item frame changes. To be used along with ``trackListFrame()``.
+    ///
+    /// It can be used with this pattern:
+    ///
+    ///     GeometryReader { geometry in
+    ///        List(0..<100) { i in
+    ///            Text("Item \(i)")
+    ///                .onItemFrameChanged(listGeometry: geometry) { (frame: CGRect?) in
+    ///                    print("rect of item \(i): \(String(describing: frame)))")
+    ///                }
+    ///        }
+    ///        .trackListFrame()
+    ///     }
+    ///
+    /// - Parameters:
+    ///   - listGeometry: A GeometryProxy from a GeometryReader wrapping your List view.
+    ///   - frameUpdated: A closure called each time an item position changes due to scrolling in the List.
+    ///   If the item goes out of List's frame, the given CGRect is nil
     func onItemFrameChanged(listGeometry: GeometryProxy, _ frameUpdated: @escaping (CGRect?) -> Void) -> some View {
         modifier(ItemFrameTracking(listGeometry: listGeometry, frameUpdated: frameUpdated))
     }
     
+    /// Makes a List view ready for use along with ``onItemFrameChanged(listGeometry:_:)``.
     func trackListFrame() -> some View {
         modifier(ListFrameTracking())
     }
